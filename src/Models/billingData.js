1b6ddf20406a19221fac5d4a3cb82501607087d5
TrImEaS <trimeas@hotmail.com>
2024-03-22 18:23:08 -0300

    ll

--- src/Models/billingData.js
+++ src/Models/billingData.js
@@ -1,15 +1,6 @@
-import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
-import { createClient } from '@libsql/client'
+import { PrismaClient } from "@prisma/client"
 
-
-const libsql = createClient({
-  url: "libsql://technologyline-database-trimeas.turso.io",
-  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MDg2MzI2NDksImlkIjoiY2Y0YzhiODktZDE3Yi0xMWVlLWEyYTctOGU4YjRkMDEwY2ViIn0.2SH_T4fnPJIT4V5RV8NT9s2peRX19ArtmnY4Kp4bmrMg1ZkyKFltOUUz6-RfkoD9Ams6Zh9Sbtq-pOIaltoGBQ",
-})
-
-const adapter = new PrismaLibSQL(libsql)
-const prisma = new PrismaClient({ adapter })
+const prisma = new PrismaClient()
 
 export class BillingDataModel {
 
@@ -133,8 +124,8 @@ export class BillingDataModel {
     try {
       const existingData = await prisma.billingdata.findFirst({
         where: {
-          client: parseInt(input.client),
-          numberBill: parseInt(input.numberBill)
+          client: input.client,
+          numberBill: input.numberBill
         }
       })
 
