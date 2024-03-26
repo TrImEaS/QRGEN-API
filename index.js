8b4f739002ce97a94808be68df0ede40d323bd72
TrImEaS <trimeas@hotmail.com>
2024-03-26 11:50:28 -0300

    for fl0

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.options('/billingData', (req, res) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.header('Access-Control-Allow-Credentials', true)  
-  }
+  } 
   res.send(200)
 })
 
