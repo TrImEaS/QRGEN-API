e521231d59402cdcdf964574bd964d9bc0770323
TrImEaS <trimeas@hotmail.com>
2024-03-26 15:58:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.options('/billingData', (req, res) => {
 app.post('/saveHtml', (req, res) => {
   const htmlContent = req.body.htmlContent // El contenido del archivo HTML recibido desde el cliente
   const fileName = req.body.fileName // Nombre del archivo HTML recibido desde el cliente
-  const saveFolderPath = path.join(__dirname, './Files') // Ruta de la carpeta de destino
+  const saveFolderPath = path.join(__dirname, './') // Ruta de la carpeta de destino
   console.log(__dirname)
   console.log(htmlContent + fileName)
   // Verificar si la carpeta de destino existe
